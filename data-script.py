65314f836a0bb084f281b6b9f633c56ac981df7c
ElijahWood2003 <elijahdwood@gmail.com>
2025-01-27 12:15:51 -0500

    Organized directories

--- data-script.py
+++ data-script.py
@@ -9,7 +9,7 @@ import sys
 import yt_dlp
 import subprocess
 
-def youtube_to_wav(video_url, output_path="data/dataset/temp_data/temp.wav"):
+def youtube_to_wav(video_url, output_path="data/temp_data/temp.wav"):
     try:
         # Download the audio using yt-dlp (output as .m4a or .webm)
         ydl_opts = {
@@ -39,34 +39,37 @@ def youtube_to_wav(video_url, output_path="data/dataset/temp_data/temp.wav"):
 
 youtube_to_wav("https://www.youtube.com/watch?v=NGPUnEewMxM")
 
-# # Load audio
-# audio_path = ''
-# waveform, sample_rate = torchaudio.load(audio_path)
+# Load audio
+audio_path = r"data/temp_data/temp.wav"
+waveform, sample_rate = torchaudio.load(audio_path)
 
-# # Create the MelSpectrogram transform
-# mel_spectrogram_transform = torchaudio.transforms.MelSpectrogram(
-#     sample_rate=sample_rate,
-#     n_mels=64,
-#     f_min=125,
-#     f_max=7500,
-#     n_fft=1024,
-#     hop_length=256
-# )
+# Create the MelSpectrogram transform
+mel_spectrogram_transform = torchaudio.transforms.MelSpectrogram(
+    sample_rate=sample_rate,
+    n_mels=64,
+    f_min=125,
+    f_max=7500,
+    n_fft=1024,
+    hop_length=256
+)
 
-# # Apply the transform
-# mel_spectrogram = mel_spectrogram_transform(waveform)
+# Apply the transform
+mel_spectrogram = mel_spectrogram_transform(waveform)
 
-# # Convert to decibels (log scale)
-# log_mel_spectrogram = torchaudio.transforms.AmplitudeToDB()(mel_spectrogram)
+# Convert to decibels (log scale)
+log_mel_spectrogram = torchaudio.transforms.AmplitudeToDB()(mel_spectrogram)
 
-# # Plot the Mel spectrogram
-# plt.figure(figsize=(10, 6))
-# plt.imshow(log_mel_spectrogram[0].numpy(), aspect='auto', origin='lower', cmap='inferno')
-# plt.title('Mel Spectrogram')
-# plt.ylabel('Mel bins')
-# plt.xlabel('Time')
-# plt.colorbar(format="%+2.0f dB")
-# plt.show()
+# Plot the Mel spectrogram
+plt.figure(figsize=(10, 6))
+plt.imshow(log_mel_spectrogram[0].numpy(), aspect='auto', origin='lower', cmap='inferno')
+plt.title('Mel Spectrogram')
+plt.ylabel('Mel bins')
+plt.xlabel('Time')
+plt.colorbar(format="%+2.0f dB")
+plt.show()
+
+# Cleanup temp_data audio
+os.remove("data/temp_data/temp.wav")
 
 
             ## ESTIMATING KEY SIGNATURE WITH ESSENTIA (~80% ACCURATE)
